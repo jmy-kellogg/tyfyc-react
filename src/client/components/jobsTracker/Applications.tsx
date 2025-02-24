b7ba62cd1964e49dedf3d2ed50a2ac132846af37
Jessica Kellogg <jmy.kellogg@gmail.com>
2025-02-23 18:12:20 -0800

    fix tab on application removal

--- src/client/components/jobsTracker/Applications.tsx
+++ src/client/components/jobsTracker/Applications.tsx
@@ -158,44 +158,48 @@ function Applications() {
             </div>
             {sortedList.map((application) => (
               <div
-                className="flex place-content-between  border-b-1 border-zinc-300 hover:bg-indigo-100 hover:cursor-pointer"
+                className="flex place-content-between border-b-1 border-zinc-300 "
                 key={application.jobId}
-                onClick={() => openApplication(application)}
               >
-                <span className="text-l p-3">
-                  <b>{application.company}</b> - {application.title}
-                </span>
-                <span className="text-l p-3">
-                  |{" "}
-                  <b
-                    className={`rounded-md p-1 ${getStatusColor(
-                      application.status
-                    )}`}
-                  >
-                    {getStatus(application.status)?.label || ""}
-                  </b>{" "}
-                  | {getFormattedDate(application.dateApplied)}
-                  <button
-                    type="button"
-                    className="rounded-md align-sub text-red-600 m-1 p-1 hover:bg-red-100"
-                    onClick={() => remove(application.jobId)}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="size-5"
+                <button
+                  className="flex place-content-between w-full hover:bg-indigo-100 hover:cursor-pointer"
+                  onClick={() => openApplication(application)}
+                >
+                  <div className="text-l p-3">
+                    <b>{application.company}</b> - {application.title}
+                  </div>
+                  <div className="text-l p-3">
+                    |{" "}
+                    <b
+                      className={`rounded-md p-1 ${getStatusColor(
+                        application.status
+                      )}`}
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                      />
-                    </svg>
-                  </button>
-                </span>
+                      {getStatus(application.status)?.label || ""}
+                    </b>{" "}
+                    | {getFormattedDate(application.dateApplied)}
+                  </div>
+                </button>
+                <button
+                  type="button"
+                  className="rounded-md align-sub text-red-600 m-1 p-1 hover:bg-red-100 hover:cursor-pointer"
+                  onClick={() => remove(application.jobId)}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                    />
+                  </svg>
+                </button>
               </div>
             ))}
             <NewJobModal />
