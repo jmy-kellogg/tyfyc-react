4556c1ea8de19db089c9f35af5adb1cbf3a6e6e2
Jessica Kellogg <jmy.kellogg@gmail.com>
2025-02-26 19:32:39 -0800

    click to add skill to resume

--- src/client/components/jobPosting/JobDescription.tsx
+++ src/client/components/jobPosting/JobDescription.tsx
@@ -1,32 +1,70 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { skillsOptions } from "../../../utils/options";
-import type { Skill } from "../../../types";
-
+import { setSkills } from "../../store/reducers/skillsSlice";
+import type { Skill, SkillsList } from "../../../types";
+import type { State } from "../../store";
 interface Props {
   description: string | null;
 }
 
 function JobDescription({ description }: Props) {
-  const skills = skillsOptions.reduce((acc: Array<string>, curr: Skill) => {
-    let newList: Array<string> = [...acc, curr.label];
-    if (curr.alias) {
-      newList = [...newList, ...curr.alias];
+  const [popover, setPopover] = useState<number | null>(null);
+  const skills: SkillsList = useSelector((state: State) => state.skills.list);
+  const dispatch = useDispatch();
+
+  const skillsAliases = skillsOptions.reduce(
+    (acc: Array<string>, curr: Skill) => {
+      let newList: Array<string> = [...acc, curr.label];
+      if (curr.alias) {
+        newList = [...newList, ...curr.alias];
+      }
+      return newList;
+    },
+    []
+  );
+
+  const addSkill = (skill: string) => {
+    const skillOption = skillsOptions.find(
+      (opt) => opt.label === skill || opt.alias?.includes(skill)
+    );
+    if (
+      skillOption &&
+      !skills.map(({ value }) => value).includes(skillOption.value)
+    ) {
+      dispatch(setSkills([...skills, skillOption]));
     }
-    return newList;
-  }, []);
+  };
 
   return (
     <p className="whitespace-pre-wrap">
-      {description?.split(" ").map((word, i) => {
-        if (skills.includes(word.replace(/[.,!?;:]$/g, ""))) {
+      {description?.split(" ").map((str, i) => {
+        const word = str.replace(/[.,!?;:]$/g, "");
+        if (skillsAliases.includes(word)) {
           return (
             <span key={i}>
-              <button className="bg-amber-100 hover:cursor-pointer">
+              <button
+                className="bg-amber-100 hover:cursor-pointer"
+                onMouseEnter={() => setPopover(i)}
+                onMouseLeave={() => setPopover(null)}
+                onClick={() => addSkill(word)}
+              >
                 {word}
+                {popover === i && (
+                  <div className="w-32 bg-white border-1 rounded-md absolute p-1">
+                    <b>Add to resume</b>
+                    <ul>
+                      {skills.map(({ label }) => (
+                        <li>{label}</li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
               </button>{" "}
             </span>
           );
         } else {
-          return `${word} `;
+          return `${str} `;
         }
       })}
     </p>
