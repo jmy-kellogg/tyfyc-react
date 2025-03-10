c8b87b36d0f5ee6d4671c76a05a38e0a501841fe
Jessica Kellogg <jmy.kellogg@gmail.com>
2025-03-09 22:04:46 -0700

    add tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import express from './express-plugin'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import express from "./express-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), express('src/server')],
+  plugins: [react(), tailwindcss(), express("src/server")],
   server: {
     port: 8000,
   },
-})
+});
