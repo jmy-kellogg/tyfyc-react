5b68dff9952ce1e82524f1a14656f410edbfbc53
Jessica Kellogg <jmy.kellogg@gmail.com>
2025-04-24 14:43:27 -0700

    add rich text editor to new application model

--- frontend/src/containers/ApplicationsList/NewJobModal.tsx
+++ frontend/src/containers/ApplicationsList/NewJobModal.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 
+import RichEditor from "@/components/RichEditor";
 import { addApplication } from "@/api/applications";
 import { getToday } from "@/utils";
 
@@ -11,6 +12,10 @@ function NewJobModal() {
   const dispatch = useDispatch();
   const [showModal, setShowModal] = useState<boolean>(false);
   const [posting, setPosting] = useState<string>("");
+
+  const setText = async (text: string) => {
+    setPosting(text);
+  };
 
   const submit = async () => {
     const reqBody: ApplicationCreate = {
@@ -49,8 +54,15 @@ function NewJobModal() {
           className="backdrop-brightness-50 overflow-y-auto overflow-x-hidden fixed z-50 w-full md:inset-0 h-full"
         >
           <div className="bg-white p-4 w-2xl h-auto rounded-lg m-auto mt-10">
-            <div className="flex items-center justify-between p-4 border-b border-slate-300 rounded-t">
-              <h3 className="text-xl font-semibold">Add New</h3>
+            <div className="flex items-center justify-between p-1 border-b border-slate-300 rounded-t">
+              <button
+                type="button"
+                className="rounded-md bg-indigo-600 text-white my-3 p-2 font-semibold shadow-md hover:cursor-pointer hover:bg-indigo-500"
+                onClick={submit}
+              >
+                Submit
+              </button>
+
               <button
                 type="button"
                 className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
@@ -73,27 +85,11 @@ function NewJobModal() {
                 </svg>
               </button>
             </div>
-            <div className="m-3">
+            <div className="m-3 min-h-100">
               <label className="block text-lg font-medium text-center">
                 Copy/Paste job Posting
               </label>
-              <textarea
-                id="posting"
-                name="posting"
-                className="w-full min-h-100 rounded-md bg-white px-3 py-1.5 text-base outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                value={posting}
-                onChange={(e) => setPosting(e.target.value)}
-              ></textarea>
-            </div>
-
-            <div className="flex items-center p-4  border-t border-slate-300">
-              <button
-                type="button"
-                className="rounded-md bg-indigo-600 text-white my-3 p-2 font-semibold shadow-md hover:cursor-pointer hover:bg-indigo-500"
-                onClick={submit}
-              >
-                Submit
-              </button>
+              <RichEditor content={posting} handleTextChange={setText} />
             </div>
           </div>
         </div>
