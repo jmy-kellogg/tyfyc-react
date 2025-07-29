a02229b4563651634442a29db91ea98f349887ec
Jessica Kellogg <jmy.kellogg@gmail.com>
2025-07-29 12:19:28 -0700

    update skill toggle

--- frontend/src/containers/profile/inputs/Skills.tsx
+++ frontend/src/containers/profile/inputs/Skills.tsx
@@ -30,8 +30,6 @@ function Skills({ lockEdit }: Props) {
   const [skillOptions, setSkillOptions] = useState<SkillSelect[]>([]);
   const [toggleSort, setToggleSort] = useState<boolean>(false);
 
-  const [open, setOpen] = useState<boolean>(false);
-
   const handleAddSkill = async (skillOptionsId: string) => {
     const skill: Skill | undefined = await addSkill({
       skillOptionsId,
@@ -52,13 +50,11 @@ function Skills({ lockEdit }: Props) {
   ) => {
     if (actionMeta.action === "select-option" && actionMeta?.option?.value) {
       await handleAddSkill(actionMeta.option.value);
-      // setHover(false);
     } else if (
       actionMeta.action === "remove-value" &&
       actionMeta?.removedValue?.id
     ) {
       await removeSkill(actionMeta.removedValue.id);
-      // setHover(false);
     }
   };
 
@@ -153,103 +149,92 @@ function Skills({ lockEdit }: Props) {
   }, [fetchSkills]);
 
   return (
-    <div>
-      <div>
-        <div className="flex items-center gap-2">
-          <h2>
-            <b>Skills</b>
-          </h2>
-          {toggleSort ? (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6 hover:cursor-pointer"
-              onClick={() => setToggleSort(!toggleSort)}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 4.5v15m7.5-7.5h-15"
-              />
-            </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6 hover:cursor-pointer"
-              onClick={() => setToggleSort(!toggleSort)}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z"
-              />
-            </svg>
-          )}
-        </div>
-        {lockEdit ? (
-          <div>
-            {toggleSort ? (
-              <div className="flex flex-wrap w-9/10">
-                <DndSort
-                  list={skills.map(formatSortList)}
-                  direction="horizontal"
-                  onSort={handleSort}
-                ></DndSort>
-              </div>
-            ) : (
-              <p>{skills.map(({ label }) => label).join(", ")}</p>
-            )}
-          </div>
+    <>
+      <div className="flex items-center gap-2">
+        <h2>
+          <b>Skills</b>
+        </h2>
+        {toggleSort ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6 text-emerald-500 rounded-lg hover:cursor-pointer hover:text-white hover:bg-emerald-300"
+            id="skill-sort-check"
+            onClick={() => setToggleSort(!toggleSort)}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m4.5 12.75 6 6 9-13.5"
+            />
+          </svg>
         ) : (
-          <>
-            {toggleSort ? (
-              <div className="flex flex-wrap w-9/10">
-                <DndSort
-                  list={skills.map(formatSortList)}
-                  direction="horizontal"
-                  onSort={handleSort}
-                ></DndSort>
-              </div>
-            ) : (
-              <CreatableSelect
-                isMulti
-                isClearable={false}
-                name="skills"
-                isSearchable
-                classNamePrefix="select"
-                className="basic-single"
-                placeholder="Technologies"
-                options={skillOptions}
-                value={skills}
-                onChange={onChange}
-                onMenuOpen={() => setOpen(true)}
-                onMenuClose={() => setOpen(false)}
-                onCreateOption={onCreateOption}
-                styles={{
-                  multiValue: (styles) => {
-                    return {
-                      ...styles,
-                      backgroundColor: "#a3b3ff",
-                      fontSize: "16px",
-                      fontWeight: "bold",
-                      borderRadius: "3px",
-                    };
-                  },
-                }}
-              />
-            )}
-          </>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6 hover:cursor-pointer rounded-lg hover:cursor-pointer hover:text-sky-500"
+            onClick={() => setToggleSort(!toggleSort)}
+            id="skill-sort-funnel"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z"
+            />
+          </svg>
         )}
       </div>
+      {toggleSort ? (
+        <div className="flex flex-wrap w-9/10">
+          <DndSort
+            list={skills.map(formatSortList)}
+            direction="horizontal"
+            onSort={handleSort}
+          ></DndSort>
+        </div>
+      ) : (
+        <>
+          {lockEdit ? (
+            <div>
+              <p>{skills.map(({ label }) => label).join(", ")}</p>
+            </div>
+          ) : (
+            <CreatableSelect
+              isMulti
+              isClearable={false}
+              name="skills"
+              isSearchable
+              classNamePrefix="select"
+              className="basic-single"
+              placeholder="Technologies"
+              options={skillOptions}
+              value={skills}
+              onChange={onChange}
+              onCreateOption={onCreateOption}
+              styles={{
+                multiValue: (styles) => {
+                  return {
+                    ...styles,
+                    backgroundColor: "#a3b3ff",
+                    fontSize: "16px",
+                    fontWeight: "bold",
+                    borderRadius: "3px",
+                  };
+                },
+              }}
+            />
+          )}
+        </>
+      )}
+
       <Divider />
-    </div>
+    </>
   );
 }
 
--- frontend/src/containers/profile/inputs/Summary.tsx
+++ frontend/src/containers/profile/inputs/Summary.tsx
@@ -27,22 +27,23 @@ function Summary({ lockEdit }: Props) {
   return (
     <>
       <h2>Summary</h2>
-
       {lockEdit ? (
         <div>
           <p>{summary}</p>
         </div>
       ) : (
-        <div className="mt-2">
-          <textarea
-            id="summary"
-            name="summary"
-            className="block w-full rounded-md bg-white px-3 py-1.5 h-32 text-base outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 text-sm/6"
-            value={summary}
-            onChange={(e) => setSummary(e.target.value)}
-            onBlur={updateData}
-          ></textarea>
-        </div>
+        <form>
+          <div className="mt-2">
+            <textarea
+              id="summary"
+              name="summary"
+              className="block w-full rounded-md bg-white px-3 py-1.5 h-32 text-base outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 text-sm/6"
+              value={summary}
+              onChange={(e) => setSummary(e.target.value)}
+              onBlur={updateData}
+            ></textarea>
+          </div>
+        </form>
       )}
       <Divider />
     </>
